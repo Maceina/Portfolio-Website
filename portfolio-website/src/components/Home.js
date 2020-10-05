66bb8fbc563dd51baf3d6e57cf9eff8f7423c9f0
Your Name <you@example.com>
2020-10-05 10:25:27 +0300

    Home page Background

--- portfolio-website/src/components/Home.js
+++ portfolio-website/src/components/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="homeStyle">
-      <h1>Padaryti curved background ir apearing letter.</h1>
+      <h1 className="text">apearing letter.</h1>
     </div>
   );
 };
