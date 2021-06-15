0636c54196fb0cde118be3f28755005ceeb80250
Maceina <maceinaaudrius@gmail.com>
2021-06-15 15:31:03 +0300

    version 3 completed

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Hero from './components/Hero';
 import Projects from './components/Projects/Projects';
 import Resume from './components/Resume/Resume';
 import AOS from 'aos';
-//123
+//1234
 import './App.css';
 
 function App() {
